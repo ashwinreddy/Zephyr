24e35c0635fda318a52cbcdcecf998bfab46c220
Ashwin Reddy <ashwin.reddy338@gmail.com>
2016-06-15 20:30:02 -0700

    cleaning up / modularizing

---
+++ src/Zephyr.cxx
@@ -0,0 +1,93 @@
+bool isFloat(string test) {
+    istringstream iss(test);
+    float f;
+    iss >> noskipws >> f;
+    return iss.eof() && !iss.fail();
+}
+
+Module::Module() {
+    defs = function_map();
+}
+
+void Module::setFunction(string s, void(*f)(Zephyr* const)) {
+    defs[s] = f;
+}
+
+function_map Module::getDefs() {
+    return defs;
+}
+
+Lexer::Lexer(string text) {
+    next = 0;
+    words = vector<string>();
+    istringstream iss(text);
+    while(iss) {
+        string word;
+        iss >> word;
+        words.push_back(word);
+    }
+}
+
+string Lexer::nextWord() {
+    return words[next++];
+}
+
+bool Lexer::nextWordExists() {
+    return next < words.size();
+}
+
+StackObj::StackObj(float f) {
+    tag = F_ACTIVE;
+    f_ = f;
+}
+
+float StackObj::getFloat() {
+    return f_;
+}
+
+int StackObj::type() {
+    return tag;
+}
+
+Zephyr::Zephyr() {
+    dictionary = function_map();
+}
+
+Zephyr* Zephyr::addModule(Module m) {
+    for (auto const& x: m.getDefs()) {
+        dictionary[x.first] = x.second;
+    }
+    return this;
+}
+
+void Zephyr::run(string source_code) {
+    Lexer lex = Lexer(source_code);
+    string word;
+    float num_val;
+
+    while(lex.nextWordExists()) {
+        word = lex.nextWord();
+        if(dictionary.find(word) != dictionary.end()) {
+            dictionary[word](this);
+        } else if (isFloat(word)) {
+            StackObj x(stod(word));
+            stack_.push(x);
+        } else if (word != "") {
+            cerr << "Unknown word" << endl;
+        }
+    }
+}
+
+stack<StackObj> Zephyr::getStack() {
+    return stack_;
+}
+
+int Zephyr::size() {
+    return stack_.size();
+}
+
+StackObj Zephyr::pop() {
+    auto t = stack_.top();
+    stack_.pop();
+    return t;
+}
---
+++ src/main.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cassert>
+#include <string>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+
+bool isFloat(string test);
+class Zephyr;
+typedef map<string, void(*)(Zephyr* const)> function_map;
+
+class Module {
+private:
+    function_map defs;
+public:
+    Module();
+    void setFunction(string s, void(*f)(Zephyr* const));
+    function_map getDefs();
+};
+
+class Lexer {
+private:
+    int next;
+    vector<string> words;
+public:
+    Lexer(string text);
+    string nextWord();
+    bool nextWordExists();
+};
+
+struct StackObj {
+    union {
+        float f_;
+    };
+    enum {F_ACTIVE = 1} tag;
+    StackObj(float f);
+    float getFloat();
+    int type();
+};
+
+class Zephyr {
+private:
+    function_map dictionary;
+    stack<StackObj> stack_;
+public:
+    Zephyr();
+    Zephyr* addModule(Module m);
+    void run(string source_code);
+    stack<StackObj> getStack();
+    int size();
+    StackObj pop();
+};
